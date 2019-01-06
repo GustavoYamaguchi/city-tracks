bc491e603eda2bafa53c26c8706d384b18cbd1f8
Gustavo Yamaguchi <gustavoyama@gmail.com>
2019-01-06 19:09:50 -0300

    Initial commit, adding track searcher

---
+++ track-searcher/src/main/java/com/track/searcher/tracksearcher/TrackSearcherApplication.java
@@ -0,0 +1,14 @@
+package com.track.searcher.tracksearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackSearcherApplication.class, args);
+	}
+
+}
+
