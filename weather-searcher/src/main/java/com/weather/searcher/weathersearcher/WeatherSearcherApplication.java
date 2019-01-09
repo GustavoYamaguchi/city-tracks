76f252150444a35565c88999ea8e771b3be641ba
Gustavo Yamaguchi <gustavoyama@gmail.com>
2019-01-09 00:41:20 -0300

    Initial commit for weather searcher

---
+++ weather-searcher/src/main/java/com/weather/searcher/weathersearcher/WeatherSearcherApplication.java
@@ -0,0 +1,14 @@
+package com.weather.searcher.weathersearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSearcherApplication.class, args);
+	}
+
+}
+
---
+++ weather-searcher/src/test/java/com/weather/searcher/weathersearcher/WeatherSearcherApplicationTests.java
@@ -0,0 +1,17 @@
+package com.weather.searcher.weathersearcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherSearcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
